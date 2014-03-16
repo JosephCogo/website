1952c45d3f5d320040242944a4cb52fae6a5522c
Joseph <joseph@cogodigital.co.nz>
2014-03-17 10:16:36 +1300

    1

--- js/app.js
+++ js/app.js
@@ -10,11 +10,12 @@ App.Router.map(function () {
     });
 });
 
-
+//class that handles authentication, not secure
+//though, as it is just using local storage, but ok 
+//for now, as it is not storing any important information
 App.AuthManager = Ember.Object.extend({
-  
   init: function() {
-    this._super();
+    this._super();//ember specific
     var accessToken = sessionStorage.token;
     if (!Ember.isEmpty(accessToken)) {
       this.authenticate(accessToken);
@@ -31,12 +32,14 @@ App.AuthManager = Ember.Object.extend({
   isAuthenticated: function() {
     return !Ember.isEmpty(sessionStorage.token);
   }
-    
 });
 
 //create a new authmanager instance to handle authentication
 var manager = App.AuthManager.create();
 
+
+//in the index route checks if the client is authenticated,
+//if so, transition to asking a question, if not go to login
 App.IndexRoute = Ember.Route.extend({
   redirect: function() {
            if(manager.isAuthenticated()){
@@ -48,32 +51,38 @@ App.IndexRoute = Ember.Route.extend({
                this.transitionTo('login');
            }
   }
-
 });
 
 App.LoginRoute = Ember.Route.extend({
+    //if user is already authenticated, go back to ask a question,
+    //little hack to make sure user doesn't go back to login
     redirect: function() {
            if(manager.isAuthenticated()){
                console.log('auth');
                this.transitionTo('askaquestion');  
            }
   }
-
 });
 
-App.LoginController = Ember.ObjectController.extend({    
+App.LoginController = Ember.ObjectController.extend({
     actions: {
+        //will create a post request to login, if successful, login and store token
         login: function () {
-            print();
             var router = this;
             $.post('https://momentum.azure-mobile.net/api/login', {
-             emailAddress: 'joemilsom@gmail.com', password: 'password'
-            }, function(data) {
-            sessionStorage.token = data.token;
-            manager.authenticate(data.token);
-                
-            router.transitionToRoute('askaquestion');  
-        });  
+                emailAddress: 'joemilsom@gmail.com', password: 'password'
+            }, function (data) {
+                sessionStorage.token = data.token;
+                manager.authenticate(data.token);
+
+                router.transitionToRoute('askaquestion');
+            });
+        },
+
+        register: function () {
+            var router = this;
+            console.log('register');
+            router.transitionToRoute('register');
         }
     }
 });
@@ -83,7 +92,4 @@ App.Team = DS.Model.extend({
     description : DS.attr('string')
 });
 
-function print(){
-        console.log(sessionStorage.token);
-}
 
