2eed6d108b35154f432c48b887dddce4f0934643
Joseph <joseph@cogodigital.co.nz>
2014-03-21 11:02:15 +1300

    1

--- js/app.js
+++ js/app.js
@@ -128,7 +128,7 @@ App.RegisterController = Ember.ObjectController.extend({
                     emailAddress: this.get('email'),
                     firstName: 'Jeff',
                     lastName: 'TheDude',
-                    skills : "test"
+                    skills : ["JavaScript", "CSS"]
                 }
             }).done(function (data) {
                 console.log('REGISTERED');
