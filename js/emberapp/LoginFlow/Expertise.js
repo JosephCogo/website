b90e0a9463c896245c012f7e0b55e4edb2bff6b7
Joseph Milsom <joseph@cogodigital.co.nz>
2014-05-07 14:24:58 +1200

    perm

--- js/emberapp/LoginFlow/Expertise.js
+++ js/emberapp/LoginFlow/Expertise.js
@@ -25,6 +25,13 @@ App.ExpertiseController = Ember.ObjectController.extend({
             //send skills through on socket, getting all the inputted tokens.
             //once successful, transition to asking a question
             addSkills($('#tokenfield-typeahead').tokenfield('getTokensList'), function () {
+                //set the permissions for ember
+                var havePermission = window.webkitNotifications.checkPermission();
+                console.log("permission " + havePermission);
+                if (havePermission != 0) {
+                    console.log(havePermission);
+                    window.webkitNotifications.requestPermission();
+                }
                 $(".application-content").fadeTo(500, 0, function () {
                     controller.replaceRoute('askaquestion');
                 });
